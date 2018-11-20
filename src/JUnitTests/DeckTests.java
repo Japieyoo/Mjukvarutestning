685c255bd72eaba675760ce8523542572dc7ca4f
Japieyoo <japieyoo@gmail.com>
2018-11-20 13:32:24 +0100

    REFACTOR

--- src/JUnitTests/DeckTests.java
+++ src/JUnitTests/DeckTests.java
@@ -3,32 +3,39 @@ package JUnitTests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import factories.DeckFactory;
 import model.Card;
 import model.Deck;
 
 class DeckTests {
+	Deck d;
+	DeckFactory factory;
+
+	@BeforeEach
+	public void setUp() {
+		factory = new DeckFactory();
+	}
 
 	@Test
 	public void getSize_FullDeck_Return52() {
-		DeckFactory factory = new DeckFactory();
 		ArrayList<Card> stubDeck = factory.getFullStubDeck();
 
-		Deck d = new Deck(stubDeck);
+		d = new Deck(stubDeck);
 		int actual = d.getSize();
 		int expected = 52;
 		assertEquals(expected, actual);
 	}
-	
+
 	@Test
 	public void Constructor_EmptyDeck_ShouldThrowError() {
-		DeckFactory factory = new DeckFactory();
 		ArrayList<Card> stubDeck = factory.getEmptyStubDeck();
 		boolean thrown = false;
-		
+
 		try {
-			Deck d = new Deck(stubDeck);
+			d = new Deck(stubDeck);
 		} catch (Exception e) {
 			thrown = true;
 		}
