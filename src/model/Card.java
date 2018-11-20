e4a72a2925fb9d8d1457324d685abe71d4172c51
Japieyoo <japieyoo@gmail.com>
2018-11-20 10:30:51 +0100

    GREEN

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,15 +1,17 @@
 package model;
 
 public class Card {
-
+	public int value;
+	
 	public Card(int i) {
 		if (i > 13 || i < 1) {
 			throw new IllegalArgumentException();
 		}
+		value = i;
 	}
 
 	public int getValue() {
-		return 1;
+		return value;
 	}
 
 }
