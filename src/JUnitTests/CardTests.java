7ec904860e4465279dace8b59c27d1fd0c066574
Japieyoo <japieyoo@gmail.com>
2018-11-20 10:19:24 +0100

    RED

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -2,19 +2,17 @@ package JUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 import org.junit.jupiter.api.Test;
 
 import model.Card;
 
 class CardTests {
 	Card c;
-	
 
 	private void setUp(int i) {
 		c = new Card(i);
 	}
-	
+
 	@Test
 	public void Constructor_Int_ShouldNotThrow() {
 		boolean thrown = true;
@@ -22,6 +20,18 @@ class CardTests {
 			setUp(1);
 		} catch (Exception e) {
 			thrown = false;
+		}
+
+		assertTrue(thrown);
+	}
+
+	@Test
+	public void Constructor_AnIntOver13_ShouldThrow() {
+		boolean thrown = false;
+		try {
+			setUp(14);
+		} catch (Exception e) {
+			thrown = true;
 		}
 
 		assertTrue(thrown);
