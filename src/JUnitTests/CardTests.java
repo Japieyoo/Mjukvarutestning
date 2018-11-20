8ee9f4ca91f984fd5c340bca23b032e4e9afce51
Japieyoo <japieyoo@gmail.com>
2018-11-20 10:02:19 +0100

    RED

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -1,14 +1,21 @@
 package JUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import model.Card;
 
 class CardTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void Constructor_Int_ShouldNotThrow() {
+		boolean thrown = true;
+		try {
+			Card c = new Card(1);
+		} catch (Exception e) {
+			thrown = false;
+		}
 
+		assertTrue(thrown);
+	}
 }
