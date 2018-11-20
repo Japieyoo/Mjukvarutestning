bc3ff6f68412a061bc5520bb0d2b7669f26fd454
Japieyoo <japieyoo@gmail.com>
2018-11-20 09:41:42 +0100

    Setting up repo

---
+++ src/JUnitTests/GameTests.java
@@ -0,0 +1,14 @@
+package JUnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/JUnitTests/PlayerTests.java
@@ -0,0 +1,14 @@
+package JUnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
