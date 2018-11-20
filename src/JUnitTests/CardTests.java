77486de177699ea0418eb99a731f4f4a75b4b68e
Japieyoo <japieyoo@gmail.com>
2018-11-20 10:21:24 +0100

    REFACTOR

--- src/JUnitTests/CardTests.java
+++ src/JUnitTests/CardTests.java
@@ -14,7 +14,7 @@ class CardTests {
 	}
 
 	@Test
-	public void Constructor_Int_ShouldNotThrow() {
+	public void Constructor_IntUnder13_ShouldNotThrow() {
 		boolean thrown = true;
 		try {
 			setUp(1);
