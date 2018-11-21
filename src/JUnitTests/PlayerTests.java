9dff68aea17be7c00571bb56a5a9ec71d6ffb865
Japieyoo <japieyoo@gmail.com>
2018-11-21 11:44:32 +0100

    RED

--- src/JUnitTests/PlayerTests.java
+++ src/JUnitTests/PlayerTests.java
@@ -1,14 +1,19 @@
 package JUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import model.Player;
 
 class PlayerTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void getScore_WhenCreated_Has0Score() {
+		Player SUT = new Player();
+		int actual = SUT.getScore();
+		int expected = 0;
+
+		assertEquals(expected, actual);
 	}
 
 }
