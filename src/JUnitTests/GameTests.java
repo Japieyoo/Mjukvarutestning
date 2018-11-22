16c3b6d3466ba957688ffa7087585420c2c6425b
Japieyoo <japieyoo@gmail.com>
2018-11-22 11:38:31 +0100

    RED

--- src/JUnitTests/GameTests.java
+++ src/JUnitTests/GameTests.java
@@ -1,14 +1,24 @@
 package JUnitTests;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.Test;
 
+import model.Deck;
+import model.Game;
+import model.Player;
+
 class GameTests {
-
 	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	public void playGame_SameCountAsCardValue_ReturnFalse() {
+		Player p = mock(Player.class);
+		Deck d = mock(Deck.class);
+		Game g = new Game(p, d);
+		when(p.getCount()).thenReturn(1);
+		when(p.checkCard(null)).thenReturn(1);
 
+		assertFalse(g.playGame(p, d));
+
+	}
 }
--- src/model/Game.java
+++ src/model/Game.java
@@ -2,4 +2,13 @@ package model;
 
 public class Game {
 
+	private Player p;
+	private Deck d;
+
+	public Game(Player player, Deck deck) {
+		p = player;
+		d = deck;
+		d.shuffle();
+	}
+
 }
